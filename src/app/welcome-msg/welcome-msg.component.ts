bfb904d322f7825bc5534e524b2581c3c07eca58
noasly <noaslyfm@gmail.com>
2018-01-19 14:33:23 +0900

    angular chapter 1

--- src/app/welcome-msg/welcome-msg.component.ts
+++ src/app/welcome-msg/welcome-msg.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-msg',
   templateUrl: './welcome-msg.component.html',
   styleUrls: ['./welcome-msg.component.css']
 })
-export class WelcomeMsgComponent implements OnInit {
+export class WelcomeMsgComponent implements OnInit, AfterViewInit {
+	title = 'app';
+  userName = 'noasly';
+  private valid = false;
+  private static CHK_KEYUP_WAIT_SEC = 5000;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  	const checkTouchedFn = () => {
+  		if(this.valid) return;
+  		alert('이름을 입력해 주세요');
+
+  		setTimeout(checkTouchedFn, WelcomeMsgComponent.CHK_KEYUP_WAIT_SEC);
+  	};
+  	// checkTouchedFn();
+  }
+
+  setName(name) {
+  	this.userName = name;
+  }
+
+  onKeyUp(name) {
+  	this.valid = name.length > 0;
   }
 
 }
