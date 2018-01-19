1bfb0c5d6994a3b737ff30f3950963aaaed37276
noasly <noaslyfm@gmail.com>
2018-01-19 11:36:47 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  userName = 'noasly';
+  private valid = false;
+  private static CHK_KEYUP_WAIT_SEC = 5000;
+
+  ngAfterViewInit() {
+  	const checkTouchedFn = () => {
+  		if(this.valid) return;
+  		alert('이름을 입력해 주세요');
+
+  		setTimeout(checkTouchedFn, AppComponent.CHK_KEYUP_WAIT_SEC);
+  	};
+  	// checkTouchedFn();
+  }
+
+  setName(name) {
+  	this.userName = name;
+  }
+
+  onKeyUp(name) {
+  	this.valid = name.length > 0;
+  }
 }
